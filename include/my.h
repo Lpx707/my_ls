33e07264f851e059ef0ae6876d5d791df0d14c16
Thomas Lpx <thomas.leparoux@epitech.eu>
2022-11-19 02:20:25 +0100

    :package: include

--- include/my.h
+++ include/my.h
@@ -11,6 +11,7 @@
     #include <stdarg.h>
     #include <string.h>
     #include <sys/types.h>
+    #include <sys/sysmacros.h>
     #include <sys/stat.h>
     #include <unistd.h>
     #include <stdlib.h>
@@ -19,9 +20,9 @@
     #include <pwd.h>
     #include <grp.h>
     #include <fcntl.h>
+    #include <stdbool.h>
 
-    typedef struct
-    {
+    typedef struct {
         char *rights;
         int nlinks;
         char *file_master;
@@ -33,36 +34,35 @@
         char *name_low;
     } ls_info;
 
-    typedef struct
-    {
+    typedef struct {
         int a;
         int l;
         int R;
     } ls_flag1;
 
-    typedef struct
-    {
+    typedef struct {
         int d;
         int r;
         int t;
     } ls_flag2;
+
     // lib for my ls
 
     void my_putchar(char c);
     void my_putstr(char const *str);
     int my_strlen(char const *str);
     void my_putnbr(int nb);
+    int my_strcmp(char const *s1, char const *s2);
     void not_dir(char *str, char *path);
 
     // my_ls
-    void my_ls(char *path);
+    void my_ls(char *path, char *file);
     void my_ls_a(char *path);
+    void my_ls_l(char *path);
     int main(int ac, char **av);
 
     // perms
 
-    void types_perms(struct stat stat_file, ls_info *info);
-    void rights_file(struct stat stat_file, ls_info *info);
 
     //check
     char *link_path(char *path, char *name);
@@ -70,4 +70,22 @@
     int is_file(char *path);
     int is_link(char *path);
     int is_special(char *path);
+    void check_file(char *path, char *file);
+
+    // my_get
+    int get_blocks(char *path);
+    int get_links(char *path);
+    char *get_user(char *path);
+    char *get_group(char *path);
+    int get_size(char *path);
+    char *get_date(char *path);
+    char *get_name(char *path);
+    char *get_time(char *path);
+    char *get_file_name(char *path);
+    char get_type(char *path);
+    char *get_permissions(char *path);
+
+    // Flag conditions
+    int check_flag(char *str);
+
 #endif /* !MY_H_ */
